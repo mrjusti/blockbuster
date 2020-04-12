b44900d75c25fd47999067bd5c633d6ca3b2e438
Hernán Kleiman <hernan.kleiman@letgo.com>
2020-04-11 23:30:51 -0400

    Return http status code if error

--- blockbuster/movies/views.py
+++ blockbuster/movies/views.py
@@ -1,18 +1,31 @@
+from http import HTTPStatus
+
 from django.http import JsonResponse
 
-from blockbuster.settings.dev import BASE_DIR
 from movies.application.use_cases import GetMovies
 from movies.application.transformers import MoviesJsonTransformer
 from movies.domain.services import GetMovies as GetMoviesService
-from movies.infrastructure.clients import GhibliClient
+from movies.infrastructure.clients import GhibliClient, BadRequestError, NotFoundError
 from movies.infrastructure.decorators import CacheMovieRepositoryDecorator
 from movies.infrastructure.repositories import GhibliMovieRepository
 
 
 def movies(request):
-    print(BASE_DIR)
     repository = CacheMovieRepositoryDecorator(GhibliMovieRepository(GhibliClient()), 'blockbuster')
     get_movies_services = GetMoviesService(repository)
     app = GetMovies(get_movies_services, MoviesJsonTransformer())
 
-    return JsonResponse(app(), safe=False)
+    try:
+        response = app()
+        code = HTTPStatus.OK
+    except BadRequestError:
+        response = {'status': HTTPStatus.BAD_REQUEST, 'message': 'Bad Request'}
+        code = HTTPStatus.BAD_REQUEST
+    except NotFoundError:
+        response = {'status': HTTPStatus.NOT_FOUND, 'message': 'Not Found'}
+        code = HTTPStatus.NOT_FOUND
+    except Exception:
+        response = {'status': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': 'Internal Server Error'}
+        code = HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return JsonResponse(response, safe=False, status=code)
